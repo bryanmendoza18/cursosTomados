bf7495b70c232e8cecf258e9a01179fdbdab6ea7
Bryan Mendoza <bryanmendoza1811@gmail.com>
2022-10-13 23:09:27 -0500

    procesandoO

--- javascript/7-objetos/48-creacionDeObjetos.js
+++ javascript/7-objetos/48-creacionDeObjetos.js
@@ -3,9 +3,14 @@
 //new == reservar nuevo espacio en memoria
 
 //se crea un objeto en memora vacio y aun notiene propiedades ni metodos asociados
+
 let persona = new Object();
 persona.nombre = 'Bryan';
 persona.direccion = '6 marzo y febres cordero';
 persona.telefono = '0987168312';
 
-console.log(persona.telefono)
+console.log(persona.telefono);
+console.log(persona.telefono);
+
+
+
