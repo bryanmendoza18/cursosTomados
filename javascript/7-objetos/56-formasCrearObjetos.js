9f3bcc1bac048b37fb4f960aabb66f64b07256ec
Bryan Mendoza <bryanmendoza1811@gmail.com>
2022-10-15 00:19:22 -0500

    Distintas formas de crear un objeto

---
+++ javascript/7-objetos/56-formasCrearObjetos.js
@@ -0,0 +1,17 @@
+//tema: Distintas formas de crear un objeto
+
+let miObjeto = new Object();
+let miObjeto2 = {};
+
+let miCadena = new String('Bryan');
+let miCadena2 = 'Bryan';
+
+let miNumero = new Number(10);
+let miNumero2 = 10;
+
+let miArray = new Array();
+let miArray2 = [];
+
+let miBoolean = new Boolean(true);
+let miBoolean2 = true;
+
