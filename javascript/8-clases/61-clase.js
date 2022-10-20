d3921e2f62249e5d5c8e1533ceef62928d0fd683
Bryan Mendoza <bryanmendoza1811@gmail.com>
2022-10-20 15:22:28 -0500

    Creando clase persona

---
+++ javascript/8-clases/61-clase.js
@@ -0,0 +1,17 @@
+//creando la clase 
+class Persona{
+    //agregando constructor
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido; 
+
+    }
+
+}
+
+//creando objeto
+let persona1 = new Persona('Bryan', 'Mendoza');
+let persona2 = new Persona('Daniela', 'Lara');
+
+console.log(persona1);
+console.log(persona2);
