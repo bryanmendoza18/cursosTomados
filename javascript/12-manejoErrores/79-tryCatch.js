4825f22ba93df1d3dcfcc15113f6082c950bc8f0
Bryan Mendoza <bryanmendoza1811@gmail.com>
2022-10-24 16:20:13 -0500

    manejo de errores

---
+++ javascript/12-manejoErrores/79-tryCatch.js
@@ -0,0 +1,17 @@
+'use strict';
+
+try{
+    let x = 10;
+}
+catch(error){
+    console.log(error);
+}
+finally{
+    console.log('Termina la revisión de errores...yo siempre me voy a ejecutar')
+}
+
+console.log('Continuamos...')
+
+
+
+
